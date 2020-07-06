1e677046dbf96a175b5015a3843e4aa085dac5ac
Ian <breaklun@yahoo.com.tw>
2020-07-06 17:36:51 +0800

    feat: create show page

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.get('/', (req, res) => {
   res.render('index', { style: "index.css", restaurants: restaurants.results })
 })
 
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurants.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', {
+    restaurant: restaurant,
+    style: "show.css"
+  })
+})
+
 // server listening on port 3000
 app.listen(3000, () => {
   console.log('This server is listening on http://localhost:3000')
